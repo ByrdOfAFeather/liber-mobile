6b2deaf5ba1c01969aaefe1b4226428deffb5574
ByrdOfAFeather <matthew_a_byrd@outlook.com>
2020-11-27 15:06:34 -0500

    Init commit

---
+++ android/app/src/main/kotlin/com/byrdofafeather/liber/liber/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.byrdofafeather.liber.liber
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/api.dart
@@ -0,0 +1,21 @@
+import 'dart/async';
+
+class Book {
+  String title;
+  String imageURL;
+  String author;
+  String edition;
+  int volume;
+}
+
+Future<List<Book>> getBooks() async {
+  return null;
+}
+
+Future<bool> addBook() async {
+  return null;
+}
+
+Future<List<Book>> searchBooks(String term) {
+  return null;
+}
